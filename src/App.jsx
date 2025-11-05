913b729788aee9c2f73d2bc5ecb9439152b3d9f6
FedericoCanale <icocanale@gmail.com>
2025-11-05 12:11:23 +0100

    add jumbotron.jpg on top of Main.jsx, paste the array with comics infos

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Main from "./components/Main.jsx";
 import BlueBar from "./components/BlueBar.jsx";
 import FooterLinks from "./components/FooterLinks.jsx";
 import Footer from "./components/Footer.jsx";
+const teachers = ["Flo", "Simo", "Man"];
 
 export default function App() {
   return (
@@ -12,6 +13,10 @@ export default function App() {
       <BlueBar />
       <FooterLinks />
       <Footer />
+
     </>
   );
 }
+
+
+
